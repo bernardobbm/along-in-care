36213f9cb0a8159edf16231b256f55b5d499cda0
Bernardo <bernardobbm334@gmail.com>
2023-09-27 23:51:30 -0300

    feat: adição do nativewind e tailwindcss e suas integrações para estilização

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './src/app/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        gray: {
+          50: '#eaeaea',
+          100: '#bebebf',
+          200: '#9e9ea0',
+          300: '#727275',
+          400: '#56565a',
+          500: '#2c2c31',
+          600: '#28282d',
+          700: '#1f1f23',
+          800: '#18181b',
+          900: '#121215',
+        },
+      },
+      plugins: [],
+    },
+  },
+}
