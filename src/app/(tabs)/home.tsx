8eabcaa79fbc3b42c7f31f0cc3e13842124d2271
Bernardo <bernardobbm334@gmail.com>
2023-09-29 16:19:11 -0300

    feat: componentes de lista e cabeçalho adicionados a página inicial

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,9 +1,29 @@
-import { Text, View } from 'react-native'
+import { Image, ScrollView, View } from 'react-native'
+import { CareList } from '../../components/CareList'
+import { Header } from '../../components/Header'
 
 export default function Home() {
+  const username = 'primeiro nome'
+
   return (
-    <View>
-      <Text>pagina</Text>
+    <View className="flex-1 bg-gray-900 px-8">
+      <Header text={`Olá, ${username}`}>
+        <Image
+          className="rounded-full"
+          alt="Profile picture of the user"
+          source={{
+            uri: 'https://i.pinimg.com/originals/56/e7/61/56e76147bc69cfabeaa726c5b944521f.jpg',
+            width: 40,
+            height: 40,
+          }}
+        />
+      </Header>
+
+      <ScrollView className="mb-6">
+        <CareList category="Medicamentos" />
+        <CareList category="Alimentação" />
+        <CareList category="Higiene" />
+      </ScrollView>
     </View>
   )
 }
