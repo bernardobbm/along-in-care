3fc79e0220ac4dde858aa23317d23bdd9dafc1d4
Bernardo <bernardobbm334@gmail.com>
2023-10-01 21:34:57 -0300

    feat: componente de dias da semana adicionado

--- src/app/(tabs)/home.tsx
+++ src/app/(tabs)/home.tsx
@@ -1,4 +1,5 @@
 import { Image, ScrollView, View } from 'react-native'
+import { CareDays } from '../../components/CareDays'
 import { CareList } from '../../components/CareList'
 import { Header } from '../../components/Header'
 
@@ -9,17 +10,17 @@ export default function Home() {
     <View className="flex-1 bg-gray-900 px-8">
       <Header text={`Olá, ${username}`}>
         <Image
-          className="rounded-full"
+          className="h-10 w-10 rounded-full"
           alt="Profile picture of the user"
           source={{
             uri: 'https://i.pinimg.com/originals/56/e7/61/56e76147bc69cfabeaa726c5b944521f.jpg',
-            width: 40,
-            height: 40,
           }}
         />
       </Header>
 
-      <ScrollView className="mb-6">
+      <CareDays />
+
+      <ScrollView className="mb-6" showsVerticalScrollIndicator={false}>
         <CareList category="Medicamentos" />
         <CareList category="Alimentação" />
         <CareList category="Higiene" />
