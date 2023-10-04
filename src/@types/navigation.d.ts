b489a4779ec461eae0190828987bb944ef6a8bbd
Bernardo <bernardobbm334@gmail.com>
2023-10-04 17:52:27 -0300

    feat: tipagens de rotas adiciondas

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,12 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Login: undefined
+      AppHome: undefined
+      NewCare: undefined
+      NewCareForm: undefined
+      Home: undefined
+      AppNewCare: undefined
+    }
+  }
+}
