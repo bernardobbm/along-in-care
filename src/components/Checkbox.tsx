1d61b1ebfeeb8b0af093bb1bf9517fa527a3ae43
Bernardo <bernardobbm334@gmail.com>
2023-10-08 13:40:49 -0300

    feat: adição de propriedade de tamanho para estlilização condicional

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -9,9 +9,15 @@ import {
 interface CheckboxProps extends TouchableOpacityProps {
   title: string
   checked?: boolean
+  size?: 'sm' | 'lg'
 }
 
-export function Checkbox({ title, checked = false, ...rest }: CheckboxProps) {
+export function Checkbox({
+  title,
+  checked = false,
+  size,
+  ...rest
+}: CheckboxProps) {
   return (
     <TouchableOpacity
       activeOpacity={0.7}
@@ -19,16 +25,26 @@ export function Checkbox({ title, checked = false, ...rest }: CheckboxProps) {
       {...rest}
     >
       {checked ? (
-        <View className="h-8 w-8 items-center justify-center rounded-lg border-2 border-[#1C6AA3] bg-[#439CDE]">
-          <Feather name="check" size={20} color={'#eaeaea'} />
+        <View
+          className={`${
+            size === 'sm' ? 'h-6 w-6' : 'h-8 w-8'
+          } items-center justify-center rounded-lg border-2 border-[#1C6AA3] bg-[#439CDE]`}
+        >
+          <Feather
+            name="check"
+            size={size === 'sm' ? 12 : 20}
+            color={'#eaeaea'}
+          />
         </View>
       ) : (
-        <View className="h-8 w-8 rounded-lg border-2 border-gray-400 bg-gray-600" />
+        <View
+          className={`${
+            size === 'sm' ? 'h-6 w-6' : 'h-8 w-8'
+          } rounded-lg border-2 border-gray-400 bg-gray-600`}
+        />
       )}
 
-      <Text className="ml-3 font-body_semibold text-base text-gray-50">
-        {title}
-      </Text>
+      <Text className="ml-3 font-body text-base text-gray-50">{title}</Text>
     </TouchableOpacity>
   )
 }
