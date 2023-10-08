23eac457ff9121ac2955e9c8cc2f0e874e12ebbf
Bernardo <bernardobbm334@gmail.com>
2023-10-08 13:45:44 -0300

    feat: configurações para a biblioteca dayjs

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native'
 
+import './src/libs/dayjs'
 import { LoginRoutes } from './src/routes/login.routes'
 
 export default function App() {
