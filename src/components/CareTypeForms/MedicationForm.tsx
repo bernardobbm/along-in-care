56dabfc2e23bf05de5ae692bd00b0b2925c80e88
Bernardo <bernardobbm334@gmail.com>
2023-10-19 00:45:27 -0300

    fix: correação de estilização do componente de dosagem

--- src/components/CareTypeForms/MedicationForm.tsx
+++ src/components/CareTypeForms/MedicationForm.tsx
@@ -79,14 +79,14 @@ export function MedicationForm() {
               render={({ field: { value, onChange } }) => (
                 <View className="w-40">
                   <TextInput
-                    className={`items-center rounded-md border-2 border-gray-400 bg-gray-600 px-4 py-3 text-center font-label text-gray-50 placeholder:text-center ${
+                    className={`items-center rounded-md border-2 border-gray-400 bg-gray-600 px-4 py-[9.5px] text-center font-label text-base text-gray-50 placeholder:text-center ${
                       errors.medication?.dosage
                         ? 'border-[#e83f5b]'
                         : 'border-gray-400'
                     }`}
                     placeholder="mg - mg/g - ml"
                     placeholderTextColor="#56565a"
-                    cursorColor={'#eaeaea'}
+                    cursorColor="#eaeaea"
                     onChangeText={onChange}
                     value={value}
                   />
