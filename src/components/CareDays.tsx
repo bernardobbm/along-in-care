2b31d736b9c71f5bfec3329043c8595c0acf8c45
Bernardo <bernardobbm334@gmail.com>
2023-10-19 00:12:32 -0300

    fix: pequenas correções na estilização

--- src/components/CareDays.tsx
+++ src/components/CareDays.tsx
@@ -1,14 +1,14 @@
 import { Text, TouchableOpacity, View } from 'react-native'
 
-const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const weekDays = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab']
 
 export function CareDays() {
   return (
-    <View className="mb-6 mt-4 flex-row justify-evenly border-b-2 border-t-2 border-gray-300 py-1">
+    <View className="mb-6 mt-4 flex-row justify-between border-b-2 border-t-2 border-gray-300 py-1">
       {weekDays.map((weekDay, index) => {
         return (
           <TouchableOpacity activeOpacity={0.7} key={`${weekDay} - ${index}`}>
-            <Text className="mx-1 text-center font-body text-xl text-gray-50">
+            <Text className="mx-1 text-center font-body text-lg text-gray-50">
               {weekDay}
             </Text>
           </TouchableOpacity>
