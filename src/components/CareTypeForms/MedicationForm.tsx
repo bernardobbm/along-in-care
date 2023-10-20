080929304ef9ddf10edd073764fb6b9cdbc68d35
Bernardo <bernardobbm334@gmail.com>
2023-10-19 22:16:59 -0300

    feat: código para adição de dosagem e validade separados em componentes próprios

--- src/components/CareTypeForms/MedicationForm.tsx
+++ src/components/CareTypeForms/MedicationForm.tsx
@@ -1,10 +1,7 @@
-import { DateTimePickerAndroid } from '@react-native-community/datetimepicker'
-import dayjs from 'dayjs'
-import { Controller, useFormContext } from 'react-hook-form'
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { useFormContext } from 'react-hook-form'
+import { View } from 'react-native'
 import { NewCareFormData } from '../../screens/NewCareForm'
 import { Form } from '../NewCareFormComponents'
-import { ErrorMessage } from '../NewCareFormComponents/ErrorMessageForm'
 import { DefaultForm } from './DefaultForm'
 
 export function MedicationForm() {
@@ -13,13 +10,6 @@ export function MedicationForm() {
     formState: { errors },
   } = useFormContext<NewCareFormData>()
 
-  const today = dayjs(new Date()).startOf('day').toDate()
-
-  const tomorrow = dayjs(today)
-    .date(today.getDate() + 1)
-    .startOf('day')
-    .toDate()
-
   return (
     <View>
       <DefaultForm>
@@ -27,77 +17,18 @@ export function MedicationForm() {
           <Form.Field>
             <Form.Label>Validade da medicação:</Form.Label>
 
-            <Controller
-              name="medication.validity"
+            <Form.MedicationForm.Validity
               control={control}
-              render={({ field: { value, onChange } }) => (
-                <View className="w-40">
-                  <TouchableOpacity
-                    onPress={() => {
-                      DateTimePickerAndroid.open({
-                        minimumDate: tomorrow,
-                        value: value ?? tomorrow,
-                        onChange: (_, date) => {
-                          if (date) onChange(date)
-                        },
-                      })
-                    }}
-                    activeOpacity={0.7}
-                    className={`items-center rounded-md border-2 border-gray-400 bg-gray-600 px-4 py-3 font-label ${
-                      errors.medication?.validity
-                        ? 'border-[#e83f5b]'
-                        : 'border-gray-400'
-                    }`}
-                  >
-                    {value ? (
-                      <Text className="font-label text-base text-gray-50">
-                        {dayjs(value).format('DD-MM-YYYY')}
-                      </Text>
-                    ) : (
-                      <Text className="font-label text-base text-gray-400">
-                        DD-MM-AAAA
-                      </Text>
-                    )}
-                  </TouchableOpacity>
-
-                  {errors.medication?.validity && (
-                    <ErrorMessage>
-                      {errors.medication.validity.message}
-                    </ErrorMessage>
-                  )}
-                </View>
-              )}
+              errors={errors.medication?.validity?.message}
             />
           </Form.Field>
 
           <Form.Field>
             <Form.Label>Dosagem:</Form.Label>
 
-            <Controller
-              name="medication.dosage"
+            <Form.MedicationForm.Dosage
               control={control}
-              render={({ field: { value, onChange } }) => (
-                <View className="w-40">
-                  <TextInput
-                    className={`items-center rounded-md border-2 border-gray-400 bg-gray-600 px-4 py-[9.5px] text-center font-label text-base text-gray-50 placeholder:text-center ${
-                      errors.medication?.dosage
-                        ? 'border-[#e83f5b]'
-                        : 'border-gray-400'
-                    }`}
-                    placeholder="mg - mg/g - ml"
-                    placeholderTextColor="#56565a"
-                    cursorColor="#eaeaea"
-                    onChangeText={onChange}
-                    value={value}
-                  />
-
-                  {errors.medication?.dosage && (
-                    <ErrorMessage>
-                      {errors.medication.dosage.message}
-                    </ErrorMessage>
-                  )}
-                </View>
-              )}
+              errors={errors.medication?.dosage?.message}
             />
           </Form.Field>
         </Form.TwoColumnField>
--- src/components/NewCareFormComponents/index.tsx
+++ src/components/NewCareFormComponents/index.tsx
@@ -7,7 +7,10 @@ import { CancelButtonForm } from './CancelButtonForm'
 import { FieldForm } from './FieldForm'
 import { IsContinuousFieldForm } from './IsContinuousFieldForm'
 import { LabelForm } from './LabelForm'
+import { Dosage } from './MedicationForm/Dosage'
+import { Validity } from './MedicationForm/Validity'
 import { RootForm } from './RootForm'
+import { ScheduleFrequency } from './ScheduleFrequency'
 import { SelectCategoryForm } from './SelectCategoryForm'
 import { SelectDateFieldForm } from './SelectDateFiedlForm'
 import { TextInputForm } from './TextInputForm'
@@ -25,10 +28,15 @@ export const Form = {
   TextInput: TextInputForm,
   SelectCategory: SelectCategoryForm,
   TwoColumnField: TwoColumnFieldForm,
+  ScheduleFrequency,
   StartsAt: SelectDateFieldForm,
   EndsAt: SelectDateFieldForm,
   IsContinuous: IsContinuousFieldForm,
   ButtonField: ButtonFieldForm,
   AffirmativeButton: AffirmativeButtonForm,
   CancelButton: CancelButtonForm,
+  MedicationForm: {
+    Validity,
+    Dosage,
+  },
 }
