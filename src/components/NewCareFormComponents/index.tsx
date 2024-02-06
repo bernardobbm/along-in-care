77e7c5aa4dddb29044b334a05fbb58afb1543c48
Bernardo <bernardobbm334@gmail.com>
2024-02-05 23:01:45 -0300

    feat: novos componentes adicionados ao objeto de componentes

--- src/components/NewCareFormComponents/index.tsx
+++ src/components/NewCareFormComponents/index.tsx
@@ -1,13 +1,11 @@
-import { Control } from 'react-hook-form'
-
-import { NewCareFormData } from '../../screens/NewCareForm'
 import { AffirmativeButtonForm } from './AffirmativeButtonForm'
 import { ButtonFieldForm } from './ButtonFieldForm'
 import { CancelButtonForm } from './CancelButtonForm'
 import { FieldForm } from './FieldForm'
 import { IsContinuousFieldForm } from './IsContinuousFieldForm'
 import { LabelForm } from './LabelForm'
-import { Dosage } from './MedicationForm/Dosage'
+import { Composition } from './MedicationForm/Composition'
+import { SelectRouteOfAdministration } from './MedicationForm/SelectRouteOfAdministration'
 import { Validity } from './MedicationForm/Validity'
 import { RootForm } from './RootForm'
 import { ScheduleFrequency } from './ScheduleFrequency'
@@ -15,11 +13,6 @@ import { SelectCategoryForm } from './SelectCategoryForm'
 import { SelectDateFieldForm } from './SelectDateFiedlForm'
 import { TextInputForm } from './TextInputForm'
 import { TwoColumnFieldForm } from './TwoColumnFieldForm'
-
-export type FormFieldHookForm = {
-  control: Control<NewCareFormData>
-  errors?: string
-}
 
 export const Form = {
   Root: RootForm,
@@ -37,6 +30,7 @@ export const Form = {
   CancelButton: CancelButtonForm,
   MedicationForm: {
     Validity,
-    Dosage,
+    Composition,
+    SelectRouteOfAdministration,
   },
 }
