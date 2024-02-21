c37c4464ad676ce2e6c70f01526dc82aa3640089
Bernardo <bernardobbm334@gmail.com>
2024-02-21 20:34:29 -0300

    feat: arquivos adicionados para novas telas da aplicação

---
+++ src/screens/Settings.tsx
@@ -0,0 +1,10 @@
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+export function Settings() {
+  return (
+    <SafeAreaView>
+      <Text>Configurações</Text>
+    </SafeAreaView>
+  )
+}
