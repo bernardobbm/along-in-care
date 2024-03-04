cff617b32620ce749cecea8756350b6b287ac070
Bernardo <bernardobbm334@gmail.com>
2024-03-04 00:10:13 -0300

    feat: formulário específico de alimentação desenvolvido

--- src/components/CareTypeForms/AlimentationForm.tsx
+++ src/components/CareTypeForms/AlimentationForm.tsx
@@ -1,11 +1,23 @@
-import { Text, View } from 'react-native'
+import { View } from 'react-native'
+import { Form } from '../NewCareFormComponents'
 import { DefaultForm } from './DefaultForm'
 
 export function AlimentationForm() {
   return (
     <View>
-      <Text className="my-4 font-body text-3xl text-gray-50">Alimentação</Text>
-      <DefaultForm />
+      <DefaultForm>
+        <Form.Field>
+          <Form.Label>Selecione a refeição:</Form.Label>
+          <Form.AlimentationForm.SelectMealType
+            mealTypes={['Café da manhã', 'Almoço', 'Janta', 'Lanche']}
+          />
+        </Form.Field>
+
+        <Form.Field>
+          <Form.Label>Alimentos:</Form.Label>
+          <Form.AlimentationForm.FoodsInput placeholder="Digite os alimentos da refeição" />
+        </Form.Field>
+      </DefaultForm>
     </View>
   )
 }
