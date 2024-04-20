d53bd74bc6aae73686f4b810d0e220ad8d8e7c25
Bernardo <bernardobbm334@gmail.com>
2024-04-20 13:11:30 -0300

    feat: componente envolvido por um link do expo router para navegação e adição de propriedades

--- src/components/CareListItem.tsx
+++ src/components/CareListItem.tsx
@@ -1,38 +1,47 @@
+import { Link } from 'expo-router'
 import { Text, TouchableOpacity, View } from 'react-native'
 
-export function CareListItem() {
+type ListItemProps = {
+  title: string
+  description: string
+  href: string
+}
+
+export function CareListItem({ title, description, href }: ListItemProps) {
   return (
-    <TouchableOpacity
-      activeOpacity={0.7}
-      className="h-24 w-fit flex-row overflow-hidden rounded-lg bg-gray-700"
-    >
-      <View className="aspect-square h-fit items-center justify-center bg-gray-600 px-3">
-        {/* //todo: colocar algum ícone aqui */}
-        <Text className="text-white">TO DO: Icon here</Text>
-      </View>
+    <Link href="/(stack)/careDetails/123" asChild>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className="h-24 w-fit flex-row overflow-hidden rounded-lg bg-gray-700"
+      >
+        <View className="aspect-square h-fit items-center justify-center bg-gray-600 px-3">
+          {/* //todo: colocar algum ícone aqui */}
+          <Text className="text-white">TO DO: Icon here</Text>
+        </View>
 
-      <View className="flex-1 justify-center px-4">
-        {/* title */}
-        <Text
-          className="font-title text-base text-gray-50"
-          ellipsizeMode="tail"
-          numberOfLines={1}
-        >
-          Medicamento Higiene Alimentação
-        </Text>
+        <View className="flex-1 justify-center px-4">
+          {/* title */}
+          <Text
+            className="font-title text-base text-gray-50"
+            ellipsizeMode="tail"
+            numberOfLines={1}
+          >
+            Medicamento Higiene Alimentação
+          </Text>
 
-        {/* description */}
-        <Text
-          className="mt-2 font-body text-base text-gray-50"
-          ellipsizeMode="tail"
-          numberOfLines={2}
-        >
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-          rem consequatur nisi culpa iusto, dolorum minima vero. Sunt eligendi
-          nisi accusamus necessitatibus, corporis itaque commodi illo. Minima
-          doloribus fugit impedit.
-        </Text>
-      </View>
-    </TouchableOpacity>
+          {/* description */}
+          <Text
+            className="mt-2 font-body text-base text-gray-50"
+            ellipsizeMode="tail"
+            numberOfLines={2}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+            rem consequatur nisi culpa iusto, dolorum minima vero. Sunt eligendi
+            nisi accusamus necessitatibus, corporis itaque commodi illo. Minima
+            doloribus fugit impedit.
+          </Text>
+        </View>
+      </TouchableOpacity>
+    </Link>
   )
 }
