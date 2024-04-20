5579fe414d25d4874a99aba3530cacc4ce52c0be
Bernardo <bernardobbm334@gmail.com>
2024-04-20 13:14:24 -0300

    feat: adição de propriedades de função e mudança em estilos

--- src/components/NoItemsRegistered.tsx
+++ src/components/NoItemsRegistered.tsx
@@ -1,24 +1,22 @@
-import { useNavigation } from '@react-navigation/native'
 import { Text, TouchableOpacity, View } from 'react-native'
 
 interface NoItemsRegisteredProps {
-  category: 'Medicamentos' | 'Alimentação' | 'Higiene'
+  category: string
+  onPress: () => void
 }
 
-export function NoItemsRegistered({ category }: NoItemsRegisteredProps) {
-  const { navigate } = useNavigation()
-
+export function NoItemsRegistered({
+  category,
+  onPress,
+}: NoItemsRegisteredProps) {
   return (
     <View className="items-center justify-center">
       <Text className="font-body text-lg text-gray-300">
-        Sem itens cadastrados em "{category}"
+        Sem itens cadastrados {category === '' ? null : `em "${category}"`}
       </Text>
 
-      <TouchableOpacity
-        activeOpacity={0.7}
-        onPress={() => navigate('AppNewCare')}
-      >
-        <Text className="font-body text-lg text-[#1C6AA3]">Adicionar</Text>
+      <TouchableOpacity activeOpacity={0.7} onPress={onPress}>
+        <Text className="font-body text-lg text-primary">Adicionar</Text>
       </TouchableOpacity>
     </View>
   )
