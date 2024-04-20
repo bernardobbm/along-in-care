2e6427c06ead2dcf331e0c57396f89678395624b
Bernardo <bernardobbm334@gmail.com>
2024-04-20 13:44:39 -0300

    feat: funcionalidade de filtragem implementada no componente

--- src/components/SelectCategoryFilter.tsx
+++ src/components/SelectCategoryFilter.tsx
@@ -1,22 +1,45 @@
+import { useState } from 'react'
 import { Text, TouchableOpacity, View } from 'react-native'
 
-export function SelectCategoryFilter() {
-  const categories = [
-    'Medicação',
-    'Higiene',
-    'Outro',
-    'Recomendações alimentares',
-  ]
+type SelectCategoryFilterProps = {
+  setFilter: (filter: string | null) => void
+}
+
+const categories = [
+  'Medicação',
+  'Higiene',
+  'Outro',
+  'Recomendações alimentares',
+]
+
+export function SelectCategoryFilter({ setFilter }: SelectCategoryFilterProps) {
+  const [selectedFilter, setSelectedFilter] = useState<number>()
 
   return (
     <View className="w-full flex-row flex-wrap rounded-xl bg-gray-700 px-2 py-3">
-      {categories.map((category) => (
+      {categories.map((category, index) => (
         <TouchableOpacity
           className="m-1 rounded-2xl bg-gray-500"
+          key={`${category}-${index}`}
           activeOpacity={0.7}
-          key={category}
+          onPress={() => {
+            if (!(selectedFilter === index)) {
+              setSelectedFilter(index)
+
+              setFilter(categories[index])
+            } else {
+              setSelectedFilter(-1)
+              setFilter(null)
+            }
+          }}
         >
-          <Text className="px-3 py-1 text-base text-gray-50">{category}</Text>
+          <Text
+            className={`px-3 py-1 text-base text-gray-50 ${
+              selectedFilter === index ? 'text-primary' : ''
+            }`}
+          >
+            {category}
+          </Text>
         </TouchableOpacity>
       ))}
     </View>
