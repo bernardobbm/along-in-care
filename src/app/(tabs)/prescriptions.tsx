aa179bc340b70ce0436562c46c30e26898a4c611
Bernardo <bernardobbm334@gmail.com>
2024-04-20 14:09:47 -0300

    feat: adição do filtro e componente de lista as telas de registros e prescrições

--- src/app/(tabs)/prescriptions.tsx
+++ src/app/(tabs)/prescriptions.tsx
@@ -1,27 +1,45 @@
-import { View } from 'react-native'
+import { useRouter } from 'expo-router'
+import { ScrollView, Text, View } from 'react-native'
 
-import { CareList } from '../../components/CareList'
+import { useState } from 'react'
+import { CareListItem } from '../../components/CareListItem'
 import { Header } from '../../components/Header'
 import { NoItemsRegistered } from '../../components/NoItemsRegistered'
 import { SelectCategoryFilter } from '../../components/SelectCategoryFilter'
 
 export default function Prescriptions() {
-  const hasItems = false
+  const [filter, setFilter] = useState<string | null>(null)
+
+  const router = useRouter()
+
+  function handleSetFilter(filter: string | null) {
+    setFilter(filter)
+  }
+
+  const hasPrescriptions = true
 
   return (
-    <View className="h-screen px-8 py-2">
+    <View className="flex-1 px-8 py-2">
       <Header text="Prescrições" />
 
-      <SelectCategoryFilter />
+      <SelectCategoryFilter setFilter={handleSetFilter} />
 
-      {hasItems ? (
-        <View className="mt-6">
-          {/* // TODO: criar um componente específico para itens de registro */}
-          <CareList title={false} category="Medicamentos" />
-        </View>
+      {hasPrescriptions ? (
+        <ScrollView className="mt-6" showsVerticalScrollIndicator={false}>
+          {filter ? (
+            <Text className="mb-5 font-body text-xl text-gray-50">
+              {filter}
+            </Text>
+          ) : null}
+
+          <CareListItem title={''} description={''} href={''} />
+        </ScrollView>
       ) : (
         <View className="h-4/6 justify-center">
-          <NoItemsRegistered category={'Medicamentos'} onPress={() => ''} />
+          <NoItemsRegistered
+            onPress={() => router.push('/newRecordForm')}
+            category={filter ?? ''}
+          />
         </View>
       )}
     </View>
