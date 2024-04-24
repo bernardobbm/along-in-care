5f3b17b80e8e3547716fa1f9521c59e65350a532
Bernardo <bernardobbm334@gmail.com>
2024-04-24 00:49:41 -0300

    refactor: especifição de telas removidas

--- src/app/(stack)/_layout.tsx
+++ src/app/(stack)/_layout.tsx
@@ -4,11 +4,7 @@ import { View } from 'react-native'
 export default function StackRoutesLayout() {
   return (
     <View className="flex-1 bg-gray-900 px-8 py-2">
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="addPatient" />
-        <Stack.Screen name="newRecordForm" />
-      </Stack>
+      <Stack screenOptions={{ headerShown: false }} />
     </View>
   )
 }
