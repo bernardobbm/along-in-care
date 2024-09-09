fc977c4b28503e7d535c0623da11d26d0303173b
Bernardo <bernardobbm334@gmail.com>
2024-09-08 21:47:24 -0300

    feat: adição de configuração mais aprofundada da api
    Adicionadas mais especificidades a instância da api, adicionando
    funções para validação e tratamento de erros e verificação
    da validade do token de autenticação.

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,5 +1,46 @@
-import axios from 'axios'
+import axios, { AxiosInstance } from 'axios'
+import { storageAuthTokenSave } from '../storage/storageAuthToken'
+import { AppError } from '../utils/AppError'
 
-export const api = axios.create({
-  baseURL: `http://192.168.1.2:3333/`,
-})
+type SignOut = () => void
+
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void
+}
+
+const api = axios.create({
+  baseURL: `http://192.168.1.3:3333/`,
+}) as APIInstanceProps
+
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    async (requestError) => {
+      if (requestError?.response?.status === 401) {
+        if (requestError.response.data?.message === 'Token inválido.') {
+          const {
+            data: { token },
+          } = await api.patch('/token/refresh')
+
+          await storageAuthTokenSave(token)
+        }
+
+        signOut()
+      }
+
+      if (requestError.response && requestError.response.data.message) {
+        return Promise.reject(new AppError(requestError.response.data.message))
+      } else {
+        return Promise.reject(
+          new AppError('Erro no servidor. Tente novamente mais tarde.'),
+        )
+      }
+    },
+  )
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager)
+  }
+}
+
+export { api }
