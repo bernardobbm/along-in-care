93ddd85349fd6bf662132463d28043deba81eabf
Bernardo <bernardobbm334@gmail.com>
2024-09-08 21:44:45 -0300

    feat: itens de validação descomentados

--- src/validations/login-form-fields-validation.ts
+++ src/validations/login-form-fields-validation.ts
@@ -1,9 +1,9 @@
 import * as yup from 'yup'
 
 export const loginFormSchema = yup.object().shape({
-  // email: yup
-  //   .string()
-  //   .email('O e-mail digitado deve ser válido')
-  //   .required('O campo "E-mail" não pode estar vazio'),
-  // password: yup.string().required('O campo "Senha" não pode estar vazio'),
+  email: yup
+    .string()
+    .email('O e-mail digitado deve ser válido')
+    .required('O campo "E-mail" não pode estar vazio'),
+  password: yup.string().required('O campo "Senha" não pode estar vazio'),
 })
