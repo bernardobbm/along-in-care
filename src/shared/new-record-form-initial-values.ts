dc018a7eb7c9dba1061370caa3cbf2b9bab70de8
Bernardo <bernardobbm334@gmail.com>
2024-12-08 19:19:46 -0300

    refactor: campos não utilizados comentados

--- src/shared/new-record-form-initial-values.ts
+++ src/shared/new-record-form-initial-values.ts
@@ -3,6 +3,6 @@ import { NewRecordFormDataType } from './interfaces/new-record-form-data-type'
 export const newRecordFormInitialValues: NewRecordFormDataType = {
   wasAccomplished: false,
   description: '',
-  hasObservations: false,
-  observations: '',
+  // observations: '',
+  // hasObservations: false,
 }
