8e0e5b0ec37b0a877ab9f7b925fef47a77c720bf
Bernardo <bernardobbm334@gmail.com>
2024-12-08 17:58:05 -0300

    feat: adicionadas requisições dos dados e condicionais para os filtros

--- src/app/(tabs)/prescriptions.tsx
+++ src/app/(tabs)/prescriptions.tsx
@@ -2,21 +2,35 @@ import { useRouter } from 'expo-router'
 import { ScrollView, Text, View } from 'react-native'
 
 import { useState } from 'react'
+import { RefreshControl } from 'react-native-gesture-handler'
 import { CareListItem } from '../../components/CareListItem'
 import { Header } from '../../components/Header'
 import { NoItemsRegistered } from '../../components/NoItemsRegistered'
 import { SelectCategoryFilter } from '../../components/SelectCategoryFilter'
+import { useCaresData } from '../../hooks/care/useCaresData'
+import { useRefreshOnFocus } from '../../hooks/useRefetchOnFocus'
+
+export function capitalizeCategory(category: string) {
+  return category === 'medicação'
+    ? 'Medicação'
+    : category === 'higiene'
+      ? 'Higiene'
+      : category === 'recomendações alimentares'
+        ? 'Recomendações Alimentares'
+        : 'Outros'
+}
 
 export default function Prescriptions() {
   const [filter, setFilter] = useState<string | null>(null)
 
   const router = useRouter()
+  const { data, refetch, isLoading } = useCaresData()
+
+  useRefreshOnFocus(refetch)
 
   function handleSetFilter(filter: string | null) {
     setFilter(filter)
   }
-
-  const hasPrescriptions = true
 
   return (
     <View className="flex-1 px-8 py-2">
@@ -24,24 +38,58 @@ export default function Prescriptions() {
 
       <SelectCategoryFilter setFilter={handleSetFilter} />
 
-      {hasPrescriptions ? (
-        <ScrollView className="mt-6" showsVerticalScrollIndicator={false}>
+      <ScrollView
+        className="mt-8"
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={isLoading}
+            onRefresh={refetch}
+            onStartShouldSetResponder={() => true}
+            colors={['#1C6AA3']}
+            progressBackgroundColor="#FFF"
+          />
+        }
+      >
+        <View className="max-h-screen">
           {filter ? (
-            <Text className="mb-5 font-body text-xl text-gray-50">
+            <Text className="mb-2 font-body text-xl text-gray-50">
               {filter}
             </Text>
           ) : null}
 
-          <CareListItem title={''} description={''} href={''} />
-        </ScrollView>
-      ) : (
-        <View className="h-4/6 justify-center">
-          <NoItemsRegistered
-            onPress={() => router.push('/newRecordForm')}
-            category={filter ?? ''}
-          />
+          {data?.cares.length === 0 ? (
+            <View className="h-96 justify-center">
+              <NoItemsRegistered
+                onPress={() => router.push('/(tabs)/newCareForm')}
+                category={filter ?? ''}
+              />
+            </View>
+          ) : (
+            data?.cares.map((care, index) =>
+              filter ? (
+                filter === capitalizeCategory(care.category) ? (
+                  <CareListItem
+                    key={`${care.title} ${index}`}
+                    category={capitalizeCategory(care.category)}
+                    title={care.title}
+                    description={care.description}
+                    id={care.id}
+                  />
+                ) : null
+              ) : (
+                <CareListItem
+                  key={`${care.title} ${index}`}
+                  category={capitalizeCategory(care.category)}
+                  title={care.title}
+                  description={care.description}
+                  id={care.id}
+                />
+              ),
+            )
+          )}
         </View>
-      )}
+      </ScrollView>
     </View>
   )
 }
--- src/app/(tabs)/records.tsx
+++ src/app/(tabs)/records.tsx
@@ -2,21 +2,40 @@ import { useRouter } from 'expo-router'
 import { useState } from 'react'
 import { ScrollView, Text, View } from 'react-native'
 
-import { CareListItem } from '../../components/CareListItem'
+import { useToast } from 'native-base'
+import { RefreshControl } from 'react-native-gesture-handler'
 import { Header } from '../../components/Header'
+import { LinkUpAPatient } from '../../components/LinkUpAPatient'
 import { NoItemsRegistered } from '../../components/NoItemsRegistered'
+import { RecordListItem } from '../../components/RecordListItem'
 import { SelectCategoryFilter } from '../../components/SelectCategoryFilter'
+import { useRecordsData } from '../../hooks/record/useRecordsData'
+import { useAuth } from '../../hooks/useAuth'
+import { useRefreshOnFocus } from '../../hooks/useRefetchOnFocus'
+import { capitalizeCategory } from './prescriptions'
 
 export default function Records() {
+  const { caregiver } = useAuth()
+
+  const { data, refetch, isLoading, error } = useRecordsData(
+    caregiver.patient ?? '',
+  )
+
+  useRefreshOnFocus(refetch)
+
   const [filter, setFilter] = useState<string | null>(null)
 
   const router = useRouter()
+
+  const toast = useToast()
 
   function handleSetFilter(filter: string | null) {
     setFilter(filter)
   }
 
-  const hasRecords = false
+  if (error) {
+    return <LinkUpAPatient />
+  }
 
   return (
     <View className="flex-1 px-8 py-2">
@@ -24,24 +43,64 @@ export default function Records() {
 
       <SelectCategoryFilter setFilter={handleSetFilter} />
 
-      {hasRecords ? (
-        <ScrollView className="mt-6" showsVerticalScrollIndicator={false}>
+      <ScrollView
+        className="mt-8"
+        showsVerticalScrollIndicator={false}
+        refreshControl={
+          <RefreshControl
+            refreshing={isLoading}
+            onRefresh={refetch}
+            onStartShouldSetResponder={() => true}
+            colors={['#1C6AA3']}
+            progressBackgroundColor="#FFF"
+          />
+        }
+      >
+        <View className="max-h-screen">
           {filter ? (
-            <Text className="mb-5 font-body text-xl text-gray-50">
+            <Text className="mb-2 font-body text-xl text-gray-50">
               {filter}
             </Text>
           ) : null}
 
-          <CareListItem title={''} description={''} href={''} />
-        </ScrollView>
-      ) : (
-        <View className="h-4/6 justify-center">
-          <NoItemsRegistered
-            onPress={() => router.push('/newRecordForm')}
-            category={filter ?? ''}
-          />
+          {data?.records.length === 0 ? (
+            <View className="h-96 justify-center">
+              <NoItemsRegistered
+                onPress={() => {
+                  router.push('/(tabs)')
+
+                  toast.show({
+                    description: 'Selecione um cuidado para criar o registro!',
+                    placement: 'top',
+                    bgColor: 'amber.500',
+                  })
+                }}
+                category={filter ?? ''}
+              />
+            </View>
+          ) : (
+            data?.records.map((record) =>
+              filter ? (
+                filter === capitalizeCategory(record.care.category) ? (
+                  <RecordListItem
+                    id={record.id}
+                    title={record.care.title}
+                    description={record.description}
+                    key={record.description}
+                  />
+                ) : null
+              ) : (
+                <RecordListItem
+                  id={record.id}
+                  title={record.care.title}
+                  description={record.description}
+                  key={record.description}
+                />
+              ),
+            )
+          )}
         </View>
-      )}
+      </ScrollView>
     </View>
   )
 }
