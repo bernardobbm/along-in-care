324b9ac7db36048c2e0221c9230a904502d176ad
Bernardo <bernardobbm334@gmail.com>
2024-12-08 19:19:11 -0300

    feat: interfaces para formulários criados na aplicação

--- src/shared/interfaces/new-record-form-data-type.ts
+++ src/shared/interfaces/new-record-form-data-type.ts
@@ -1,6 +1,6 @@
 export type NewRecordFormDataType = {
   wasAccomplished: boolean
   description: string
-  hasObservations: boolean
-  observations: string
+  // hasObservations: boolean
+  // observations: string
 }
