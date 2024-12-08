fcf4997b22c536bfa98010c76f67237781f7b5df
Bernardo <bernardobbm334@gmail.com>
2024-12-08 17:58:57 -0300

    feat: adicionadas opções nas configurações e suas devidas rotas

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,16 +1,62 @@
-import { Text, TouchableOpacity, View } from 'react-native'
+import { FontAwesome, FontAwesome5, Ionicons } from '@expo/vector-icons'
+import { View } from 'react-native'
 
+import { useRouter } from 'expo-router'
 import { Header } from '../../components/Header'
+import { SettingMenuButton } from '../../components/SettingMenuButton'
+import { useAuth } from '../../hooks/useAuth'
 
 export default function Settings() {
+  const router = useRouter()
+
+  const { signOut } = useAuth()
+
   return (
-    <View className="h-screen px-8 py-2">
+    <View className="h-screen bg-gray-900 px-8 py-2">
       <Header text="Configurações" />
 
-      <View>
-        <TouchableOpacity activeOpacity={0.7}>
-          <Text className="text-gray-50">Apagar conta</Text>
-        </TouchableOpacity>
+      <View className="h-3/4 justify-between">
+        <View className="mt-6 h-48 w-fit justify-between rounded-xl bg-gray-700 px-6 py-5">
+          <SettingMenuButton
+            onPress={() => router.push('/(stack)/profile')}
+            icon={
+              <FontAwesome name="user-circle-o" size={30} color="#eaeaea" />
+            }
+            href="/(stack)/profile"
+            text="Perfil"
+          />
+
+          <SettingMenuButton
+            onPress={() => router.push('/(stack)/patient')}
+            icon={
+              <FontAwesome5
+                name="hand-holding-medical"
+                size={30}
+                color="#eaeaea"
+              />
+            }
+            href="/(stack)/patient"
+            text="Paciente"
+          />
+
+          <SettingMenuButton
+            onPress={signOut}
+            icon={<Ionicons name="exit-outline" size={34} color="#e83f5b" />}
+            href="/"
+            text="Sair"
+            color="danger"
+          />
+        </View>
+
+        <View className="w-11/12 items-center">
+          <SettingMenuButton
+            onPress={signOut}
+            icon={<Ionicons name="trash" size={28} color="#e83f5b" />}
+            href="/"
+            text="Excluir conta"
+            color="danger"
+          />
+        </View>
       </View>
     </View>
   )
