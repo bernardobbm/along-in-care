ac26c2641a9ef20311837fb587c129210fcfe0b3
Bernardo <bernardobbm334@gmail.com>
2024-12-08 19:16:59 -0300

    feat: adição de funções para verificação, armazenamento de tokens e pausar requisições enquanto isso

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,8 +1,16 @@
-import axios, { AxiosInstance } from 'axios'
-import { storageAuthTokenSave } from '../storage/storageAuthToken'
+import axios, { AxiosError, AxiosInstance } from 'axios'
+import {
+  storageAuthTokenGet,
+  storageAuthTokenSave,
+} from '../storage/storageAuthToken'
 import { AppError } from '../utils/AppError'
 
 type SignOut = () => void
+
+type PromiseType = {
+  onSuccess: (token: string) => void
+  onFailure: (error: AxiosError) => void
+}
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void
@@ -12,19 +20,98 @@ const api = axios.create({
   baseURL: `http://192.168.1.3:3333/`,
 }) as APIInstanceProps
 
+let failedQueue: PromiseType[] = []
+let isRefreshing = false
+
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
     (response) => response,
     async (requestError) => {
       if (requestError?.response?.status === 401) {
-        if (requestError.response.data?.message === 'Token inválido.') {
-          const {
-            data: { token },
-          } = await api.patch('/token/refresh')
+        if (
+          requestError.response.data.message === 'Token inválido.' ||
+          requestError.response.data.message === 'Token expirado.'
+        ) {
+          const { refresh_token } = await storageAuthTokenGet()
 
-          await storageAuthTokenSave(token)
+          if (!refresh_token) {
+            signOut()
+
+            return Promise.reject(requestError)
+          }
+
+          const originalRequestConfig = requestError.config
+
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({
+                onSuccess: (token) => {
+                  originalRequestConfig.headers = {
+                    Authorization: `Bearer ${token}`,
+                  }
+
+                  resolve(api(originalRequestConfig))
+                },
+                onFailure: (error) => {
+                  reject(error)
+                },
+              })
+            })
+          }
+
+          isRefreshing = true
+
+          // eslint-disable-next-line no-async-promise-executor
+          return new Promise(async (resolve, reject) => {
+            try {
+              const {
+                data: { token, refreshToken },
+              } = await api.patch(
+                '/token/refresh',
+                {},
+                {
+                  headers: {
+                    Authorization: `Bearer ${refresh_token}`,
+                  },
+                },
+              )
+
+              await storageAuthTokenSave({ token, refresh_token: refreshToken })
+
+              if (originalRequestConfig.data) {
+                originalRequestConfig.data = JSON.parse(
+                  originalRequestConfig.data,
+                )
+              }
+
+              originalRequestConfig.headers = {
+                Authorization: `Bearer ${token}`,
+              }
+
+              api.defaults.headers.common.Authorization = `Bearer ${token}`
+
+              failedQueue.forEach((request) => {
+                request.onSuccess(token)
+              })
+
+              resolve(api(originalRequestConfig))
+            } catch (err) {
+              if (err instanceof AxiosError) {
+                failedQueue.forEach((request) => {
+                  request.onFailure(err)
+                })
+
+                signOut()
+                return reject(err)
+              }
+
+              throw err
+            } finally {
+              isRefreshing = false
+              failedQueue = []
+            }
+          })
         }
-
         signOut()
       }
 
