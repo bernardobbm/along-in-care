8798a339db044a491de7e58e92cb7242f4e60cb5
Bernardo <bernardobbm334@gmail.com>
2024-12-08 17:24:53 -0300

    feat: adição da requisição e uso dos dados recebidos

--- src/app/(stack)/careDetails/[id].tsx
+++ src/app/(stack)/careDetails/[id].tsx
@@ -1,48 +1,43 @@
 import { AntDesign } from '@expo/vector-icons'
+import dayjs from 'dayjs'
+import { useLocalSearchParams, useRouter } from 'expo-router'
 import { ScrollView, Text, View } from 'react-native'
 
-import { useRouter } from 'expo-router'
-import { CareDays } from '../../../components/CareDays'
+import { Menu, useToast } from 'native-base'
+import { CareDaysInfo } from '../../../components/CareDaysInfo'
 import { AlimentationDetails } from '../../../components/CareDetailsComponents/AlimentationDetails'
 import { HygieneDetails } from '../../../components/CareDetailsComponents/HygieneDetails'
 import { MedicationDetails } from '../../../components/CareDetailsComponents/MedicationDetails'
+import { HamburgerMenu } from '../../../components/HamburgerMenu'
 import { Header } from '../../../components/Header'
 import { HeaderButton } from '../../../components/HeaderButton'
 import { Form } from '../../../components/NewCareFormComponents'
-import { NewCareFormData } from '../../../shared/interfaces/new-care-form-data-type'
+import { useCareDetailData } from '../../../hooks/care/useCareDetailData'
+import { shouldBeAdministeredToday } from '../../../utils/should-be-administred-today'
+import { verifyIfIsInAValidInterval } from '../../../utils/verify-if-is-in-a-valid-interval'
 
 export default function CareDetails() {
-  // const { } = useLocalSearchParams()
+  const { id } = useLocalSearchParams()
+
   const router = useRouter()
 
-  const care: NewCareFormData = {
-    careDays: [0, 3, 6],
-    category: 'recomendações alimentares',
-    title: 'Cuidado Gigatônico',
-    description: '',
-    frequency: 'diariamente',
-    startTime: null,
-    scheduleType: 'variable',
-    interval: '',
-    startsAt: null,
-    endsAt: null,
-    isContinuous: false,
-    medication: {
-      administrationRoute: 'Oral',
-      quantity: '5',
-      unit: 'ml',
-    },
-    hygiene: {
-      hygieneCategory: 'Banho',
-      instructions:
-        ' tomar banho tomar banho tomar banho tomar banho tomar banho tomar banho tomar banho tomar banho tomar banho tomar banho tomar banho tomar banho',
-    },
-    alimentation: {
-      meal: 'Almoço',
-      food: 'pedra tijolo cimento paralelepipedo',
-      notRecommendedFood: 'qualquer coisa comestivel',
-    },
+  const toast = useToast()
+
+  const { data } = useCareDetailData(id as string)
+
+  if (!data?.care) {
+    toast.show({
+      description: 'Algo deu errado, tente novamente mais tarde!',
+      bgColor: 'danger.700',
+      placement: 'top',
+    })
+
+    return router.back()
   }
+
+  const weekDays = data?.care.weekDays.map(({ week_day }) => {
+    return week_day
+  })
 
   return (
     <ScrollView
@@ -50,67 +45,99 @@ export default function CareDetails() {
       showsVerticalScrollIndicator={false}
     >
       <Header
-        text={care.title}
+        text={data?.care?.title || 'Não encontrado'}
         button={
           <HeaderButton
             icon={<AntDesign name="arrowleft" color={'#eaeaea'} size={24} />}
             onPress={() => router.back()}
           />
         }
-      />
+      >
+        <HamburgerMenu>
+          {verifyIfIsInAValidInterval(data.care.starts_at, data.care.ends_at) &&
+          shouldBeAdministeredToday(data.care.weekDays) ? (
+            <Menu.Item
+              onTouchEnd={() =>
+                router.replace(`/(stack)/newRecordForm/${data?.care.id}`)
+              }
+            >
+              <AntDesign name="addfile" size={20} color="#22c55e" />
+              <Text className="text-green-500">Criar registro</Text>
+            </Menu.Item>
+          ) : (
+            <></>
+          )}
 
-      <CareDays />
+          <Menu.Item onTouchEnd={() => {}}>
+            <AntDesign name="delete" size={20} color="#e83f5b" />
+            <Text className="break-words text-primaryRed">Apagar cuidado</Text>
+          </Menu.Item>
+        </HamburgerMenu>
+      </Header>
+
+      <CareDaysInfo careDays={weekDays || []} />
 
       <Form.Field>
         <Form.Label>Descrição</Form.Label>
         <Text className="font-body text-xl text-gray-300">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-          tenetur aspernatur nihil animi odio eveniet minus reprehenderit ab,
-          repudiandae debitis excepturi architecto dignissimos porro,
-          accusantium deserunt aliquid officia maxime voluptas.
+          {data?.care?.description}
         </Text>
       </Form.Field>
 
       <Form.Field>
         <Form.Label>Realizar o cuidado:</Form.Label>
 
-        <View className="flex-row items-center space-x-16">
+        <View className="flex-row items-center">
           <Text className="w-44 font-body text-xl text-gray-300">
-            Sempre no mesmo horário
+            {data?.care?.schedule_type === 'variable'
+              ? 'A cada: '
+              : 'Todo dia às: '}
           </Text>
 
-          <Text className="font-body text-xl text-gray-300">18:00 horas</Text>
+          <Text className="font-body text-xl text-gray-300">
+            {data?.care.interval} horas
+          </Text>
         </View>
       </Form.Field>
 
       <Form.TwoColumnField>
         <Form.Field>
           <Form.Label>Data de inicio:</Form.Label>
-          <Text className="font-body text-2xl text-gray-300">10/11/2020</Text>
+          <Text className="font-body text-2xl text-gray-300">
+            {dayjs(data?.care.starts_at).format('DD/MM/YYYY')}
+          </Text>
         </Form.Field>
 
         <Form.Field>
           <Form.Label>Data de encerramento:</Form.Label>
-          <Text className="font-body text-2xl text-gray-300">11/12/2020</Text>
+          <Text className="w-40 font-body text-2xl text-gray-300">
+            {data?.care.ends_at
+              ? dayjs(data.care.ends_at).format('DD/MM/YYYY')
+              : 'Não há data de encerramento'}
+          </Text>
         </Form.Field>
       </Form.TwoColumnField>
 
-      {care.category === 'medicação' ? (
+      {data?.care?.category === 'medicação' ? (
         <MedicationDetails
-          administrationRoute={care.medication.administrationRoute}
-          quantity={Number(care.medication.quantity)}
-          unit={care.medication.unit}
+          administrationRoute={
+            data?.care?.medication?.administration_route || ''
+          }
+          quantity={Number(data?.care?.medication?.quantity)}
+          unit={data?.care?.medication?.unit || ''}
         />
-      ) : care.category === 'higiene' ? (
+      ) : data?.care?.category === 'higiene' ? (
         <HygieneDetails
-          hygieneCategory={care.hygiene.hygieneCategory}
-          instructions={care.hygiene.instructions}
+          hygieneCategory={data?.care?.hygiene?.hygiene_category || ''}
+          instructions={data?.care?.hygiene?.instructions || ''}
         />
-      ) : care.category === 'recomendações alimentares' ? (
+      ) : data?.care?.category === 'recomendações alimentares' ? (
         <AlimentationDetails
-          meal={care.alimentation.meal}
-          food={care.alimentation.food}
-          notRecommendedFood={care.alimentation.notRecommendedFood}
+          meal={data?.care?.alimentation?.meal || ''}
+          food={data?.care?.alimentation?.food || ''}
+          notRecommendedFood={
+            data?.care?.alimentation?.not_recommended_food || ''
+          }
         />
       ) : null}
     </ScrollView>
