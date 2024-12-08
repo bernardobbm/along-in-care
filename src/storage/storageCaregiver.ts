61a51b9e444c470d48e04a8437280200e26310b2
Bernardo <bernardobbm334@gmail.com>
2024-12-08 19:22:08 -0300

    feat: comparação dos dados salvos com novos para atualização

--- src/storage/storageCaregiver.ts
+++ src/storage/storageCaregiver.ts
@@ -6,6 +6,22 @@ export async function storageCaregiverSave(caregiver: CaregiverDTO) {
   await AsyncStorage.setItem(CAREGIVER_STORAGE, JSON.stringify(caregiver))
 }
 
+export async function storageCaregiverUpdateInfo(
+  caregiver: Partial<CaregiverDTO>,
+) {
+  const storage = await AsyncStorage.getItem(CAREGIVER_STORAGE)
+
+  const caregiverStored: CaregiverDTO = storage ? JSON.parse(storage) : {}
+
+  const updatedCaregiverToStore: CaregiverDTO = {
+    name: caregiver.name || caregiverStored.name,
+    email: caregiver.email || caregiverStored.email,
+    patient: caregiver.patient || caregiverStored.patient,
+  }
+
+  await storageCaregiverSave(updatedCaregiverToStore)
+}
+
 export async function storageCaregiverGet() {
   const storage = await AsyncStorage.getItem(CAREGIVER_STORAGE)
 
