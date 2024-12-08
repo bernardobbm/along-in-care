b8aa0f34e4b50482913ede71d09cf2a0c18f4922
Bernardo <bernardobbm334@gmail.com>
2024-12-08 18:07:36 -0300

    feat: funções para recarregar requisições ao back-end adicionadas

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,7 @@
-import { SplashScreen, Stack } from 'expo-router'
-import { useCallback } from 'react'
+import NetInfo from '@react-native-community/netinfo'
+import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useCallback, useEffect } from 'react'
 
 import {
   Inter_400Regular,
@@ -12,8 +14,16 @@ import {
   RobotoCondensed_400Regular,
   useFonts,
 } from '@expo-google-fonts/roboto-condensed'
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+  focusManager,
+  onlineManager,
+} from '@tanstack/react-query'
 import { StatusBar } from 'expo-status-bar'
 import { NativeBaseProvider } from 'native-base'
+import { AppState, AppStateStatus, Platform } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { AuthContextProvider } from '../contexts/AuthContext'
 
@@ -28,6 +38,26 @@ export default function AuthRoutesLayout() {
     Inter_700Bold,
   })
 
+  useEffect(() => {
+    onlineManager.setEventListener((setOnline) => {
+      return NetInfo.addEventListener((state) => {
+        setOnline(!!state.isConnected)
+      })
+    })
+  }, [])
+
+  function onAppStateChange(status: AppStateStatus) {
+    if (Platform.OS !== 'web') {
+      focusManager.setFocused(status === 'active')
+    }
+  }
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener('change', onAppStateChange)
+
+    return () => subscription.remove()
+  }, [])
+
   const onLayoutRootView = useCallback(() => {
     if (hasLoadedFonts) {
       SplashScreen.hideAsync()
@@ -38,18 +68,24 @@ export default function AuthRoutesLayout() {
     return null
   }
 
+  const client = new QueryClient({
+    queryCache: new QueryCache(),
+  })
+
   return (
     <SafeAreaView className="flex-1 bg-gray-900" onLayout={onLayoutRootView}>
-      <NativeBaseProvider>
-        <AuthContextProvider>
-          <StatusBar style="light" translucent backgroundColor="#1C6AA3" />
+      <QueryClientProvider client={client}>
+        <NativeBaseProvider>
+          <AuthContextProvider>
+            <StatusBar style="light" translucent backgroundColor="#1C6AA3" />
 
-          <Stack screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="index" />
-            <Stack.Screen name="(tabs)" />
-          </Stack>
-        </AuthContextProvider>
-      </NativeBaseProvider>
+            <Stack screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="index" />
+              <Stack.Screen name="(tabs)" />
+            </Stack>
+          </AuthContextProvider>
+        </NativeBaseProvider>
+      </QueryClientProvider>
     </SafeAreaView>
   )
 }
